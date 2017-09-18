f95c88b08c6ab428c4af348a6dd93db1cbc578f0
Erik McComb <erik@erikmccomb.com>
2017-09-18 16:40:33 -0600

    navbar and nomatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import {Route, Switch} from 'react-router-dom'; 
+
 
 class App extends Component {
   render() {
     return (
-      <Route 
-        exact path='/'
-        component={Home}
-      />
+      <div>
+        <Switch>
+          <Route 
+            exact path='/'
+            component={Home}
+          />
+          <Route path='/about' component={about} />
+          <Route component={NoMatch}/>
+        </Switch>
+      </div>
     );
   }
 }
